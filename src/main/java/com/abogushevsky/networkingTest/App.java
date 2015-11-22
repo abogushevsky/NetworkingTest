9f6b3d62b0f3e293072318ed9cd07e8d17c92087
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-11-22 19:20:34 +0300

    args check added

--- src/main/java/com/abogushevsky/networkingTest/App.java
+++ src/main/java/com/abogushevsky/networkingTest/App.java
@@ -1,5 +1,7 @@
 package com.abogushevsky.networkingTest;
 
+import java.net.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	if (args.length > 0 && args[0] == "-s") {
+    		System.out.println("Server mode");
+    	} else {
+    		System.out.println("Client mode");
+    		
+    		try {
+    			InetAddress addr = InetAddress.getLocalHost();
+    		} catch (UnknownHostException e) {
+    			// TODO Auto-generated catch block
+    			System.out.println("UnknownHostException occured");
+    			e.printStackTrace();
+    		}
+    	}    	
     }
 }
