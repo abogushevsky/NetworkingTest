69502d0eb04e16f39e585ab7a3da6bf9eb2009f9
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-11-22 19:38:08 +0300

    Server implementation added

--- src/main/java/com/abogushevsky/networkingTest/App.java
+++ src/main/java/com/abogushevsky/networkingTest/App.java
@@ -1,5 +1,7 @@
 package com.abogushevsky.networkingTest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
 
 /**
@@ -8,20 +10,38 @@ import java.net.*;
  */
 public class App 
 {
+	private static final int PORT = 50001;
+	private static final int SERVER_BACKLOG = 10;
+	
     public static void main( String[] args )
     {
-    	if (args.length > 0 && args[0] == "-s") {
-    		System.out.println("Server mode");
-    	} else {
-    		System.out.println("Client mode");
-    		
-    		try {
-    			InetAddress addr = InetAddress.getLocalHost();
-    		} catch (UnknownHostException e) {
-    			// TODO Auto-generated catch block
-    			System.out.println("UnknownHostException occured");
-    			e.printStackTrace();
-    		}
-    	}    	
+    	try {
+	    	if (args.length > 0 && args[0] == "-s") {
+	    		System.out.println("Server mode");
+	    		try (ServerSocket srvSocket = new ServerSocket(PORT, SERVER_BACKLOG, InetAddress.getLocalHost())) {
+	    			System.out.println(String.format("Listening %d", SERVER_BACKLOG));
+	    			while(true) {
+	    				Socket sock = srvSocket.accept();
+	    				System.out.println(String.format("Data recieved from %s", sock.getInetAddress().toString()));
+	    				InputStream in = sock.getInputStream();
+	    				int c;
+	    				while((c = in.read()) != -1) {
+	    					System.out.print((char) c);
+	    				}	    				
+	    				sock.close();
+	    				System.out.println("\nConnection closed.\n");
+	    			}
+	    		}
+	    	} else {
+	    		System.out.println("Client mode");
+    			InetAddress addr = InetAddress.getLocalHost();	    		
+	    	} 
+    	} catch (UnknownHostException e) {
+			System.out.println("UnknownHostException occured");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
     }
 }
