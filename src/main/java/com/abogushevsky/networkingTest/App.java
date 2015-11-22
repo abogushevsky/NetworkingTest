f62bd6edbfb0cdce2f0c857cbc189ee8021bba94
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-11-22 19:48:59 +0300

    Server & Client logic moved to separated methods

--- src/main/java/com/abogushevsky/networkingTest/App.java
+++ src/main/java/com/abogushevsky/networkingTest/App.java
@@ -15,26 +15,20 @@ public class App
 	
     public static void main( String[] args )
     {
+    	if (args.length > 1) {
+    		System.out.println("Wrong args number!");
+    		return;
+    	}
+    	
     	try {
 	    	if (args.length > 0 && args[0] == "-s") {
-	    		System.out.println("Server mode");
-	    		try (ServerSocket srvSocket = new ServerSocket(PORT, SERVER_BACKLOG, InetAddress.getLocalHost())) {
-	    			System.out.println(String.format("Listening %d", SERVER_BACKLOG));
-	    			while(true) {
-	    				Socket sock = srvSocket.accept();
-	    				System.out.println(String.format("Data recieved from %s", sock.getInetAddress().toString()));
-	    				InputStream in = sock.getInputStream();
-	    				int c;
-	    				while((c = in.read()) != -1) {
-	    					System.out.print((char) c);
-	    				}	    				
-	    				sock.close();
-	    				System.out.println("\nConnection closed.\n");
-	    			}
-	    		}
+	    		runAsServer();
 	    	} else {
-	    		System.out.println("Client mode");
-    			InetAddress addr = InetAddress.getLocalHost();	    		
+	    		if (args.length == 1 || args[0].length() == 0) {
+	    			runAsClient(null);
+	    		} else {
+	    			runAsClient(args[1]);
+	    		}
 	    	} 
     	} catch (UnknownHostException e) {
 			System.out.println("UnknownHostException occured");
@@ -42,6 +36,35 @@ public class App
 		} catch (IOException e) {
 			System.out.println("IOException");
 			e.printStackTrace();
+		}
+    }
+    
+    private static void runAsServer() throws UnknownHostException, IOException {
+    	System.out.println("Server mode");
+		try (ServerSocket srvSocket = new ServerSocket(PORT, SERVER_BACKLOG, InetAddress.getLocalHost())) {
+			System.out.println(String.format("Listening %d", SERVER_BACKLOG));
+			while(true) {
+				Socket sock = srvSocket.accept();
+				System.out.println(String.format("Data recieved from %s", sock.getInetAddress().toString()));
+				InputStream in = sock.getInputStream();
+				int c;
+				while((c = in.read()) != -1) {
+					System.out.print((char) c);
+				}	    				
+				sock.close();
+				System.out.println("\nConnection closed.\n");
+			}
+		}
+    }
+    
+    private static void runAsClient(String hostName) throws IOException {
+    	System.out.println("Client mode");
+		InetAddress addr = hostName == null ? InetAddress.getLocalHost() : InetAddress.getByName(hostName);
+		String data = "";
+		while(data != ":q") {
+			try (Socket sock = new Socket(addr, PORT)) {
+				
+			}
 		}
     }
 }
